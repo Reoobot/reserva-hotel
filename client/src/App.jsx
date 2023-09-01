7bf347b0c986dfa7458be0499a1abcf427b20838
Freddy Vegas <vegasfalexis@gmail.com>
2023-09-01 21:50:56 +0200

    second commit after erre the first porject

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import AccountPage from './pages/ProfilePage'
 import ProfilePage from './pages/ProfilePage'
 import PlacesPage from './pages/PlacesPage'
 import PlacesFormPage from './pages/PlacesFormPage'
+import PlacePage from './pages/PlacePage'
 
 
 //ojo con locahot3000
@@ -31,6 +32,7 @@ function App() {
           <Route path='/account/places' element={<PlacesPage/>}/>
           <Route path='/account/places/new' element={<PlacesFormPage/>}/>
           <Route path='/account/places/:id' element={<PlacesFormPage/>}/>
+          <Route path='/place/:id' element={<PlacePage/>}/>
          
           </Route>
         </Routes>   
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 export default function IndexPage(){
@@ -8,18 +9,18 @@ const [places,setPlaces] = useState([])
   useEffect(()=>{
     //nota recuerda modificar la api y la imag por 4000 solo
     axios.get('/places').then(response=>{
-      setPlaces([...response.data,...response.data,...response.data,...response.data])
-      console.log('prueba',response.data);
+      setPlaces([...response.data])
+      // console.log('prueba',response.data);
     })
   },[])
     return(
         <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
          {places.length > 0 && places.map(place=>(
-          <div>
-            <div className="bg-gray-500 mb-2 rounded-2xl flex">
+          <Link to = {'/place/'+place._id}>
+            <div className="bg-gray-500 mb-2 rounded-2xl flex"  >
               {place.photos?.[0] && (
                 <img 
-                key={place.photos[0]}
+                key={place._id}
                   className="rounded-2xl object-cover aspect-square" src={'http://localhost:4001/uploads/'+place.photos?.[0]} alt="" />
                 )}
             </div>
@@ -29,7 +30,7 @@ const [places,setPlaces] = useState([])
                 <span className="font-bold"> Є{place.price} </span>
                per night
               </div>
-          </div>
+          </Link>
          ))}
         </div> 
     );
